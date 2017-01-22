3e9a497d1d0e5cb2f48c0c5a5deeb3e112a3b2e0
Anton Horvath <horvath@digitaldocuments.org>
2017-01-22 20:25:59 +0100

    dddd

---
+++ MVCApp/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var Movie = new Movie() {Name = "Shrek"};
+
+            return View(Movie);
+        }
+    }
+}
